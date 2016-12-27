cfa758397eafb477b5b3622ee322ebafb2806119
Tang Rufus <tangrufus@gmail.com>
2016-12-28 01:26:16 +0800

    wp scaffold plugin medium-remove-cross-links

---
+++ medium-remove-cross-links.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Medium Remove Cross Links
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     medium-remove-cross-links
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Medium_Remove_Cross_Links
+ */
