3bd3d447a663c40dc75132e4845e82a3e0f3d675
Tang Rufus <tangrufus@gmail.com>
2017-03-12 17:57:34 +0800

    Wait for elements to prevent race conditions

--- tests/acceptance/SanityCheckCept.php
+++ tests/acceptance/SanityCheckCept.php
@@ -7,6 +7,9 @@ $I->amOnPluginsPage();
 $I->deactivatePlugin('remove-medium-cross-links');
 
 $I->amOnpage('/');
+$I->waitForElement('p.site-description', 10);
+$I->scrollTo(['css' => 'h2.entry-title']);
+
 $I->click('Acceptance Test for rmcl');
 
 $I->see('Also published on Medium.');
