b94af71c5deb1d502abc24ad39c98a67f45b9fe5
Tang Rufus <tangrufus@gmail.com>
2017-04-25 02:05:29 +0800

    Apply code style

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,79 +13,79 @@
  * @see https://wordpress.org/plugins/remove-medium-cross-links/
  */
 
-module.exports = function ( grunt ) {
+module.exports = function (grunt) {
 
-	'use strict';
+    'use strict';
 
-	// Project configuration
-	grunt.initConfig( {
+    // Project configuration
+    grunt.initConfig({
 
-		pkg: grunt.file.readJSON( 'package.json' ),
+        pkg: grunt.file.readJSON('package.json'),
 
-		addtextdomain: {
-			options: {
-				textdomain: '<%= pkg.name %>',
-				updateDomains: true
-			},
-			target: {
-				files: {
-					src: [
-						'*.php',
-						'src/**/*.php',
-						'vendor/**/*.php'
-					]
-				}
-			}
-		},
+        addtextdomain: {
+            options: {
+                textdomain: '<%= pkg.name %>',
+                updateDomains: true
+            },
+            target: {
+                files: {
+                    src: [
+                        '*.php',
+                        'src/**/*.php',
+                        'vendor/**/*.php'
+                    ]
+                }
+            }
+        },
 
-		makepot: {
-			target: {
-				options: {
-					include: [
-						'.*.php',
-						'src/.*',
-						'vendor/.*'
-					],
-					mainFile: '<%= pkg.name %>.php',
-					potHeaders: {
-						poedit: true,
-						'Project-Id-Version': '<%= pkg.name %> <%= pkg.version %>',
-						'language-team': '<%= pkg.pot.languageteam %>',
-						'last-translator': '<%= pkg.pot.lasttranslator %>',
-						'report-msgid-bugs-to': '<%= pkg.pot.reportmsgidbugsto %>'
-					},
-					type: 'wp-plugin',
-					updateTimestamp: true
-				}
-			}
-		},
+        makepot: {
+            target: {
+                options: {
+                    include: [
+                        '.*.php',
+                        'src/.*',
+                        'vendor/.*'
+                    ],
+                    mainFile: '<%= pkg.name %>.php',
+                    potHeaders: {
+                        poedit: true,
+                        'Project-Id-Version': '<%= pkg.name %> <%= pkg.version %>',
+                        'language-team': '<%= pkg.pot.languageteam %>',
+                        'last-translator': '<%= pkg.pot.lasttranslator %>',
+                        'report-msgid-bugs-to': '<%= pkg.pot.reportmsgidbugsto %>'
+                    },
+                    type: 'wp-plugin',
+                    updateTimestamp: true
+                }
+            }
+        },
 
-		// Bump version numbers
-		version: {
-			changelog: {
-				options: {
-					prefix: 'future-release='
-				},
-				src: ['.github_changelog_generator']
-			},
-			php: {
-				options: {
-					prefix: '\\* Version:\\s+'
-				},
-				src: ['<%= pkg.name %>.php']
-			},
-			readme: {
-				options: {
-					prefix: 'Stable tag:\\s+'
-				},
-				src: ['README.txt']
-			}
-		}
+        // Bump version numbers
+        version: {
+            changelog: {
+                options: {
+                    prefix: 'future-release='
+                },
+                src: ['.github_changelog_generator']
+            },
+            php: {
+                options: {
+                    prefix: '\\* Version:\\s+'
+                },
+                src: ['<%= pkg.name %>.php']
+            },
+            readme: {
+                options: {
+                    prefix: 'Stable tag:\\s+'
+                },
+                src: ['README.txt']
+            }
+        }
 
-	} );
+    });
 
-	require( 'load-grunt-tasks' )( grunt );
+    require('load-grunt-tasks')(grunt);
 
-	grunt.util.linefeed = '\n';
+    grunt.util.linefeed = '\n';
 
 };
